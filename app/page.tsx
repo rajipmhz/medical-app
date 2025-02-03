00f71c8cb1b2d02fe54d5a2668886bd37a524b32
Rajip <RajipMhz@gmail.com>
2025-02-03 19:53:21 +0545

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+import React from "react"; 
+export default function Home(){
+  <section className="text-3xl flex min-h-screen justify-center items-center">
+    <h2 className="font-semibold">Medical App</h2>
+  </section>
+}
